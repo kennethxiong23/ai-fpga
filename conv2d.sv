96b70f25580ca47da1d59f55c40af3a797d1cf5f
eneri4 <irenehong123@gmail.com>
2025-12-16 18:49:24 -0500

    Convolution module done, need to debug

---
+++ conv2d.sv
@@ -0,0 +1,76 @@
+module conv2d #(
+    parameter int kernel_size,
+    parameter int stride,
+    parameter int input_width,
+    parameter int output_width
+)(
+    input  logic clk,
+    input  logic reset,
+    input  logic [kernel_size*kernel_size-1:0] filter,
+    input  logic signed [input_width*input_width*32-1:0]    input_image,
+    output logic signed [output_width*output_width*32-1:0]  output_image,
+    output logic done
+);
+
+    int output_x = 0;
+    int output_y = 0;
+
+    logic signed [63:0] kernel_sum;
+    logic signed [63:0] weighted_val;
+
+    int base_x;
+    int base_y;
+
+    int input_x;
+    int input_y;
+
+    always_comb begin
+        kernel_sum = 64'b0;
+
+        base_x = output_x * stride;
+        base_y = output_y * stride;
+
+        for (int ky = 0; ky < kernel_size; ky++) begin
+            for (int kx = 0; kx < kernel_size; kx++) begin
+
+                int input_x = base_x + kx;
+                int input_y = base_y + ky;
+
+                if (input_x < 0 || input_x >= input_width || input_y < 0 || input_y >= input_width) begin
+                    weighted_val = 64'b0;
+                end else begin
+                    weighted_val = input_image[(input_y*input_width + input_x)*32 +: 32] * filter[(ky*kernel_size) + kx];
+                end
+
+                kernel_sum += weighted_val;
+            end
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            output_x <= 0;
+            output_y <= 0;
+            done     <= 0;
+        end else begin
+
+            if (!done) begin
+                output_image[(output_y*output_width + output_x)*32 +: 32] <= kernel_sum[31:0];
+
+                if (output_x == output_width-1) begin
+                    output_x <= 0;
+                    if (output_y == output_width-1) begin
+                        output_y <= 0;
+                        done <= 1;
+                    end else begin
+                        output_y <= output_y + 1;
+                    end
+                end else begin
+                    output_x <= output_x + 1;
+                end
+            end
+
+        end
+    end
+
+endmodule
--- top.sv
+++ top.sv
@@ -1,23 +1,36 @@
 `include "memory.sv"
+`include "conv2d.sv"
 `include "relu.sv"
 `include "maxpool2d.sv"
 
 module top #(
     parameter image_width = 5,
-    parameter pooled_width = 3
+    parameter pooled_width = 3,
+    parameter kernel_width = 3,
+    parameter stride = 2,
+    parameter filtered_width = 2
 )(
     input logic clk
 );
     logic relu_reset = 0;
     logic max_pool_reset = 0;
+    logic conv_reset = 0;
+
     logic signed[image_width*image_width*32-1:0] input_image;
+    
     logic relu_done;
     logic max_pool_done;
+    logic conv_done;
 
-    logic signed[image_width*image_width*32-1:0] test;
-    logic [image_width*image_width*32-1:0] output_test;
-
+    logic signed [filtered_width*filtered_width*32-1:0] filtered_output;
+    logic [image_width*image_width*32-1:0] relu_output;
     logic signed [pooled_width*pooled_width*32-1:0] pooled_output;
+
+    logic [kernel_width*kernel_width-1:0] filter;
+
+    initial begin
+        filter = 9'b101010101;
+    end
 
     memory #(
         .INIT_FILE      ("zero_padding.txt"),
@@ -25,13 +38,26 @@ module top #(
     ) u1 (
         .read_data      (input_image)
     );
+    conv2d #(
+        .kernel_size    (kernel_width),
+        .stride         (stride),
+        .input_width    (image_width),
+        .output_width   (filtered_width)
+    ) conv2d (
+        .clk            (clk),
+        .reset          (conv_reset),
+        .filter         (filter),
+        .input_image    (input_image),
+        .output_image   (filtered_output),
+        .done           (conv_done)
+    );
     relu #(
         .map_width       (image_width)
     ) relu (
-        .reset          (relu_reset),
+        .reset           (relu_reset),
         .clk             (clk),
         .input_map       (input_image),
-        .output_map      (output_test),
+        .output_map      (relu_output),
         .done            (relu_done)
     );
     max_pool2d #(

3b15d583d723ca5eb703d19f5430ffc54a2e8934
eneri4 <irenehong123@gmail.com>
2025-12-15 23:53:54 -0500

    Processing full images using relu

--- relu.sv
+++ relu.sv
@@ -1,7 +1,39 @@
-module relu
+module relu #(
+    parameter int map_width
+)
 (
-    input  int  in_relu,
-    output int  out_relu
+    input logic clk,
+    input logic signed [map_width*map_width*32-1:0] input_map,
+    output logic signed [map_width*map_width*32-1:0] output_map,
+    output logic done
 );
-    assign  out_relu = (in_relu > 0) ? in_relu : 0;
+
+int x = 0;
+int y = 0;
+int idx;
+initial begin
+done = 0;
+end
+always_ff @(posedge clk) begin
+
+    if (!done) begin
+        idx <= (x*map_width + y) * 32; 
+        output_map[idx +:32] <= 
+            (input_map[idx +:32] > 0) ? input_map[idx +:32] : 32'd0;
+        
+        if (x == map_width-1 && y == map_width-1) begin
+            x <= 0;
+            y <= 0;
+        end else if (y == map_width-1) begin
+            y <= 0;
+            x <= x+1;
+        end else begin
+            y <= y+1;
+        end
+    end
+
+end
+
+
 endmodule
+
